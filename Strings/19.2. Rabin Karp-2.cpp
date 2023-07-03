1dacd60750a6b939f52ba0dc15daf3caf61256fd
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-07-03 11:10:27 +0530

    Medium

---
+++ Strings/19.2. Rabin Karp-2.cpp
@@ -0,0 +1,51 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define d 10
+void rabinKarp(char pattern[], char text[], int q){
+    int m=strlen(pattern);
+    int n=strlen(text);
+    int i,j;
+    int p=0;
+    int t=0;
+    int h=1;
+    
+    for(i=0;i<m-1;i++){
+        h=((h*d) % q);
+    }
+    
+    //hash value for pattern
+    for(i=0;i<m;i++){
+        p=(d*p +pattern[i])%q;
+        t=(d*t +text[i])%q;
+    }
+    //find the match
+    for(i=0;i<=n-m;i++){
+        if(p==t){
+            for(j=0;j<m;j++){
+                if(text[i+j]!=pattern[j]){
+                    break;
+                }
+            }
+            if(j==m){
+                cout<<"pattern is found at position "<<i+1<<endl;
+            }
+        }
+        if(i<n-m){
+            t=(d*(t-text[i]*h)+text[i+m])%q;
+            if(t<0){
+                t=(t+q);
+            }
+        }
+    }
+}
+int main() {
+    char text[] = "ABCCDDAEFG";
+  char pattern[] = "CDD";
+  int q = 13;
+  rabinKarp(pattern, text, q);
+
+    return 0;
+}
