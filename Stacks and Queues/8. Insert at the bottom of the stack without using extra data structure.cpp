d62f5162fe58cb6927da3b2871b1cae3fff444ea
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-07-05 12:17:02 +0530

    Easy

---
+++ Stacks and Queues/8. Insert at the bottom of the stack without using extra data structure.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+void solve(stack<int> &myStack,int x){
+    if(myStack.empty()){
+        myStack.push(x);
+        return ;
+    }
+
+    int ele=myStack.top();
+    myStack.pop();
+    solve(myStack,x);
+    myStack.push(ele);
+}
+
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    solve(myStack,x);
+    return myStack;
+}
