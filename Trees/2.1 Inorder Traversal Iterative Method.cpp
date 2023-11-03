b69c7cf5b3a30cf2b7c3808c184acf1da1c2c3a3
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-11-03 18:25:09 +0530

    Easy-imp

---
+++ Trees/2.1 Inorder Traversal Iterative Method.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        TreeNode *curr=root;
+        stack<TreeNode*> st;
+        while(true){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                if(st.empty()){
+                    break;
+                }
+                curr=st.top();
+                ans.push_back(curr->val);
+                st.pop();
+                curr=curr->right;
+            }
+        }
+        return ans;
+    }
+};
